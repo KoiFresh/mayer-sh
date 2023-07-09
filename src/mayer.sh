f711cc85f920b75b9cb22d0c5427e91635c09057
Kai Mayer <kai.mayer@t-online.de>
2023-07-09 13:19:55 +0200

    inital commit

---
+++ src/mayer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# --------------------------------------------------------
+# Script: mayer.sh
+# Description: prints useless information about the author
+# Usage: ./mayer.sh
+# Author: Kai mayer
+# Version: 0.0.1
+# Date: 2023-09-07
+# --------------------------------------------------------
+
+echo "Willkommen auf meiner Website!"
