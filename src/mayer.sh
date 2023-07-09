10e89472aea0c902b528f6c92947b1ebb0a1dfb4
Kai Mayer <kai.mayer@t-online.de>
2023-07-09 20:27:35 +0200

    feat: add justify colors and add info to script

--- src/mayer.sh
+++ src/mayer.sh
@@ -9,10 +9,17 @@
 # Date: 2023-09-07
 # --------------------------------------------------------
 
-echo "Willkommen auf meiner Website!"
-echo "\n\r"
-echo "Script ausführen: '$ curl https://mayer.sh/mayer.sh -o mayer.sh && bash ./mayer.sh'" 
-echo "Hier kommt noch mehr!"
-echo "..."
 
-$ curl https://mayer.sh/mayer.sh -o mayer.sh && bash ./mayer.sh
+echo "Hey hier sind ein paar infos über den Author und dieses Script:"
+echo "
++-------------------------------------------------------------+
+|                                                             |
+| Script:      mayer.sh                                       |
+| Description: prints useless information about the author    |
+| Usage:       ./mayer.sh                                     |
+| Author:      Kai Mayer <kai@mayer.sh>                       |
+| Version:     0.0.1                                          |
+| Date:        2023-09-07                                     |
+|                                                             |
++-------------------------------------------------------------+
+"
