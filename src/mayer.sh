9d33f41c2f49f334ea32d7f34fe59f4d9a22fb74
Kai Mayer <kai.mayer@t-online.de>
2023-07-09 14:13:46 +0200

    feat: add html preview for script

--- src/mayer.sh
+++ src/mayer.sh
@@ -4,9 +4,15 @@
 # Script: mayer.sh
 # Description: prints useless information about the author
 # Usage: ./mayer.sh
-# Author: Kai mayer
+# Author: Kai Mayer
 # Version: 0.0.1
 # Date: 2023-09-07
 # --------------------------------------------------------
 
 echo "Willkommen auf meiner Website!"
+echo "\n\r"
+echo "Script ausführen: '$ curl https://mayer.sh/mayer.sh -o mayer.sh && bash ./mayer.sh'" 
+echo "Hier kommt noch mehr!"
+echo "..."
+
+$ curl https://mayer.sh/mayer.sh -o mayer.sh && bash ./mayer.sh
