4e256b832ecb37d0bcbb4fd35925e4c7e1b64210
Kai Mayer <kai.mayer@t-online.de>
2023-07-10 08:57:46 +0200

    inital commit

---
+++ mayer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# --------------------------------------------------------
+# Script: mayer.sh
+# Description: prints useless information about the author
+# Usage: ./mayer.sh
+# Author: Kai Mayer
+# Version: 0.0.1
+# Date: 2023-09-07
+# --------------------------------------------------------
+
+
+echo "Hey hier sind ein paar infos über den Author und dieses Script:"
+echo "
++-------------------------------------------------------------+
+|                                                             |
+| Script:      mayer.sh                                       |
+| Description: prints useless information about the author    |
+| Usage:       ./mayer.sh                                     |
+| Author:      Kai Mayer <kai@mayer.sh>                       |
+| Version:     0.0.1                                          |
+| Date:        2023-09-07                                     |
+|                                                             |
++-------------------------------------------------------------+
+"
