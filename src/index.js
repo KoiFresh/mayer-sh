85ce52d646484b36159970201134bfcd314795f3
Kai Mayer <kai.mayer@t-online.de>
2023-07-11 15:11:35 +0200

    fix: set background rotation based on current time

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 function main() {
+	const seconds = (Date.now() / 1000).toFixed(0);
+	const degrees = ((seconds % 200) * (360 / 200)).toFixed(2);
+
 	const bg = htmx.find(".background")
-	bg.style.transform = `rotate(${Math.floor(Math.random() * 360)}deg)`
+	bg.style.transform = `rotate(${degrees}deg)`
 }
 
 window.onload = main;
