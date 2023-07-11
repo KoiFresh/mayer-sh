2b11c294095a2d7ed68e872300399dc0a24e2dee
KoiFresh <60545012+KoiFresh@users.noreply.github.com>
2023-07-11 13:45:28 +0000

    Build: (0b33b81) Merge pull request #4 from KoiFresh/fix/bg-rotation
    fix: background rotation center

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function main() {
 	const seconds = (Date.now() / 1000).toFixed(0);
-	const degrees = ((seconds % 200) * (360 / 200)).toFixed(2);
+	const degrees = ((seconds % 200) * (360 / 200));
 
 	const bg = htmx.find(".background")
 	bg.style.transform = `rotate(${degrees}deg)`
