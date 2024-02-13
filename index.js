68823439f450c1cde7e3d2cd2cd63c7889bca7aa
KoiFresh <kai@mayer.sh>
2024-02-13 10:29:10 +0000

    Build: (c1ea40a) Merge pull request #6 from KoiFresh/fix/ci-deploy-folder
    fix: deploy dist folder instead of src

--- index.js
+++
@@ -1,9 +0,0 @@
-function main() {
-	const seconds = (Date.now() / 1000).toFixed(0);
-	const degrees = ((seconds % 200) * (360 / 200));
-
-	const bg = htmx.find(".background")
-	bg.style.transform = `rotate(${degrees}deg)`
-}
-
-window.onload = main;
